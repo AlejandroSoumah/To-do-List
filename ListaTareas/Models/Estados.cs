3f0081871d4d6f27875b4b5619bb02caed01efd1
AlejandroSoumah <alejandrosoumah@gmail.com>
2018-10-27 19:06:48 +0200

    new class Estados

---
+++ ListaTareas/Models/Estados.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ListaTareas.Models
+{
+    public class Estados
+    {
+        public String Estado { get; set; }
+        public int IdEstado { get; set; }
+
+        public Estados()
+        {
+        }
+        
+    }
+}
